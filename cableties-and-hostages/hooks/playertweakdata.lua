2bbac5dc9efdaee074b9c63355b21cd320d9b640
theokrueger <krueger.theo@gmail.com>
2021-09-28 20:22:07 -0400

    complete rewrite, version 2.0

---
+++ cableties-and-hostages/hooks/playertweakdata.lua
@@ -0,0 +1,15 @@
+if not _G.CTH then
+	dofile(ModPath .. 'scripts/setup.lua')
+end
+
+-- set maximum following hostages to n (based on settings)
+Hooks:PostHook(
+	PlayerTweakData,
+	'init',
+	'CTH_PTD_init',
+	function(self, tweak_data)
+		if _G.LuaNetworking:IsHost() then
+			self.max_nr_following_hostages = CTH.settings.cth_increasefollowers or 1 -- excessive nil check
+		end
+	end
+)
---
+++ cableties-and-hostages/scripts/settings.lua
@@ -0,0 +1,30 @@
+_G.CTH = _G.CTH or {}
+
+-- default settings
+CTH.settings = {
+	['cth_infiniteties'] = true,
+	['cth_hidehud'] = false,
+	['cth_increasefollowers'] = 3
+}
+
+-- save and load function
+function CTH:savesettings()
+	local f = io.open(CTH.savefile, 'w+')
+	if f then
+		f:write(json.encode(CTH.settings))
+		f:close()
+	end
+end
+
+function CTH:loadsettings()
+	local f = io.open(CTH.savefile, 'r')
+	if f then
+		local tbl = json.decode(f:read('*all'))
+		if tbl ~= nil and type(tbl) == 'table' then
+			for k, v in pairs(tbl) do
+				CTH.settings[k] = v
+			end
+		end
+		f:close()
+	end
+end
